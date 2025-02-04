618d35600a8c0a7c9dbfcd9f2b3717d8d3c8e148
Nikita <n1k_1ta@bk.ru>
2025-02-04 23:20:41 +0500

    font_commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="content" style={{ backgroundColor: "#1F1B1C" }}>
+    <div className="content" style={{ backgroundColor: "#1F1B1C", fontFamily: "Dudka, sans-serif" }}>
       <h1>Home</h1>
     </div>
   );
